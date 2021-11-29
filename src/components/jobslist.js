1e6eb53a3f892a5cf8a190647a36bfaadf8bd130
Zharet Bautista Montes <zd.bautista@uniandes.edu.co>
2021-11-29 17:49:57 -0500

    Primera parte

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -10,6 +10,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      visits: 4352,
     },
     {
       id: "0002",
@@ -18,6 +19,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      visits: 1250,
     },
     {
       id: "0003",
@@ -26,6 +28,7 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      visits: 134,
     },
   ]);
 
@@ -40,6 +43,7 @@ const JobsList = () => {
             <th scope="col">Salary</th>
             <th scope="col">City</th>
             <th scope="col">Publication date</th>
+            <th scope="col">Number of Visits</th>
           </tr>
         </thead>
         <tbody>
